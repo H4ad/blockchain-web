080ac88c8823084e6f08fe263d7cd01d9e520511
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-10-12 18:09:51 +0000

    Arrumando redirecionamento para o inicio

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -26,7 +26,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/blockchain';
+    protected $redirectTo = '/inicio';
 
     /**
      * Create a new controller instance.
